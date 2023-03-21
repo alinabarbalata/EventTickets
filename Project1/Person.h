7349ac77d1241a492ffb62662954b65ad0962414
alinabarbalata <barbalataalina21@stud.ase.ro>
2023-03-21 16:51:28 +0200

    getters and setters for Person and Birthday classes

---
+++ Project1/Person.h
@@ -0,0 +1,93 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class Birthday {
+	int day;
+	int month;
+	int year;
+public:
+	Birthday() {
+		this->day = 1;
+		this->month = 1;
+		this->year = getCurrentYear() - 18;
+	}
+	Birthday(int day, int month, int year) {
+		setDay(day);
+		setMonth(month);
+		setYear(year);
+	}
+	void setDay(int day) {
+		if (day < 1 || day>32) {
+			throw exception ("The day is inexistent!");
+		}
+		this->day = day;
+	}
+	void setMonth(int month) {
+		if (month < 1 || month>12) {
+			throw exception("The month is inexistent!");
+		}
+		this->month = month;
+	}
+	void setYear(int year) {
+		if (year > (getCurrentYear() - 18) || year < (getCurrentYear() - 100)) {
+			throw exception("Invalid year!");
+		}
+		this->year = year;
+	}
+	int getDay() {
+		return this->day;
+	}
+	int getMonth() {
+		return this->month;
+	}
+	int getYear() {
+		return this->year;
+	}
+	string getBirthday_string() {
+		return to_string(this->day) + "-" + to_string(this->month) + "-" + to_string(this->year);
+	}
+
+	static int getCurrentYear() {
+		time_t now = time(0);
+		tm* ltm = localtime(&now);
+		return (1900 + ltm->tm_year);
+	}
+	/*static string intToString(int int_var) {
+		stringstream stream;
+		stream >> int_var;
+		string string_var;
+		stream << string_var;
+	}*/
+
+};
+class Person {
+	int idPerson;
+	string first_name;
+	string last_name;
+	Birthday dateOfBirth;
+public:
+	static int COUNTER;
+public:
+	Person(string first_name, string last_name, int day_birth, int month_birth, int year_birth):first_name(first_name),last_name(last_name) {
+		this->dateOfBirth = Birthday(day_birth, month_birth, year_birth);
+		COUNTER++;
+		this->idPerson = COUNTER;
+	}
+	string getFullName() {
+		return this->first_name+" "+last_name;
+	}
+	string getBirthday() {
+		return this->dateOfBirth.getBirthday_string();
+	}
+	~Person() {
+		COUNTER--;
+	}
+	int getId() {
+		return this->idPerson;
+	}
+};
+int Person::COUNTER = 0;
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <ctime>
+#include "Person.h";
 using namespace std;
 
 void main() {
-	cout << "Hello";
+	//Tester for Birthday class
+	Birthday b1(1, 3, 2003);
+	cout << b1.getDay() << "." << b1.getMonth() << "." << b1.getYear()<<endl;//works
+	//Birthday b2(-1, 12, 2003);//does not work because variable day does not verify condition
+	//Birthday b3(1, 15, 2003);//does not work because variable month does not verify condition
+	//Birthday b4(1, 12, 2006);//does not work because variable year does not verify condition
+	cout << b1.getBirthday_string()<<endl;
+
+	//Tester for Person class
+	Person p1("Andreea", "Alexandra", 1, 3, 2002);
+	cout<<p1.getBirthday()<<endl;
+	cout << p1.getFullName()<<endl;
 }
